37069ee70f7ab8c8e359144c641c1ac4ddeb6b03
K-Karalius <kristupaskaralius1@gmail.com>
2023-07-22 20:36:35 +0300

    update

--- src/main/java/example/loanCalculator/Main.java
+++ src/main/java/example/loanCalculator/Main.java
@@ -1,25 +1,7 @@
 package example.loanCalculator;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-
-import java.io.IOException;
-
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Loan Calculator");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
+public class Main {
+    public static void main(String[] args){
+        App.main(args);
     }
 }
